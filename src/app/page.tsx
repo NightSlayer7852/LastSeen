0c24e69c244bff9535ae67a9838f1bc6768e0332
NightSlayer7852 <tanawadepranav@gmail.com>
2026-01-01 19:45:35 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md space-y-8">
+        <div className="border border-zinc-800 bg-zinc-900/50 backdrop-blur-md"><div className="space-y-5">
+          <div className="space-y-2">Create Room</div>
+        </div> </div>
+
+      </div>
+
+    </main>
+  );
+}
